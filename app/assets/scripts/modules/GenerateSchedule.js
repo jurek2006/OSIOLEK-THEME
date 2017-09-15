879b81dca5d49af25f3e0615b60b0b4db5a43774
jurek2006 <jurek2006@gmail.com>
2017-09-15 17:24:20 +0200

    Utworzono generowanie danych testowego repertuaru

--- app/assets/scripts/modules/GenerateSchedule.js
+++ app/assets/scripts/modules/GenerateSchedule.js
@@ -4,14 +4,46 @@ class GenerateSchedule{
 	constructor(){
 		const moviesDB = new MoviesDB();
 		moviesDB.generateDB();
-		moviesDB.console();
 
+		const schedule = new Schedule(moviesDB.movies);
+		schedule.generateSchedule();
+
+		console.log(schedule);
 	}
 }
 
-class MoviesSchedule{
-	constructor(){
+class Schedule{
+	constructor(movies){
+		this.movies = movies;
+		this.schedule = {};
+	}
 
+	addProjection(day, movieSlug, time, if3d, languageVer, specProjLabel){
+	// dodaje projekcję dla filmu (w dniu day, film o movieSlug)
+		
+		// sprawdzenie czy istnieje film o podanym slug w movies (zdefiniowanej bazie filmów)
+		const movieObj = this.movies[movieSlug];
+		if(movieObj !== undefined && movieObj !== null){
+		// jeśli znaleziono odpowiedni film, dodanie do niego projekcji i dodanie referencji do filmu do schedule dla danego dnia
+
+			movieObj.addProjection(day, time, if3d, languageVer, specProjLabel);
+
+			// sprawdzenie czy dla danego dnia jest już jakaś projekcja (jeśli nie, to trzeba dla niego utworzyć pustą tablicę)
+			if(this.schedule[day] === undefined || this.schedule[day] === null){
+				this.schedule[day] = [];
+			}
+			// dodanie w schedule do pola dnia referencji do obiektu filmu
+			this.schedule[day].push(movieObj);
+
+		} else {
+			console.log(movieSlug + " nie jest poprawnym slug'iem filmu w Schedule.addProjection() [GenerateSchedule.js]");
+		}
+	}
+
+	generateSchedule(){
+		this.addProjection('2017-09-01', 'anabelle', '12:00', false, 'dubbing');
+		this.addProjection('2017-09-01', 'bodyguard', '15:00', false, 'dubbing');
+		this.addProjection('2017-09-01', 'bodyguard', '20:00', true, 'napisy');
 	}
 }
 
@@ -20,9 +52,9 @@ class MoviesDB{
 		this.movies = {}
 	}
 
-	addMovie(slug, title, image, runtime, ageCat, genre, projections, specialLabel){
+	addMovie(slug, title, image, runtime, ageCat, genre, specialLabel){
 	// metoda dodająca film do bazy (this.movies)
-		this.movies[slug] = new MovieBox( slug, title, image, runtime, ageCat, genre, projections, specialLabel);
+		this.movies[slug] = new Movie( slug, title, image, runtime, ageCat, genre, specialLabel);
 	}
 
 	generateDB(){
@@ -64,16 +96,27 @@ class MoviesDB{
 	}
 }
 
-class MovieBox{
-	constructor(slug, title, image, runtime, ageCat, genre, projections, specialLabel){
+class Movie{
+	constructor(slug, title, image, runtime, ageCat, genre, specialLabel){
 		this.slug = slug;
 		this.title = title;
 		this.image = image;
 		this.runtime = runtime;
 		this.ageCat = ageCat;
 		this.genre = genre;
-		this.projections = projections;
+		this.projections = {};
 		this.specialLabel = specialLabel;
+	}
+
+	addProjection(day, time, if3d, languageVer, specProjLabel){
+
+		// sprawdzenie czy są już projekcje dla danego dnia - jeśli nie to trzeba utworzyć dla niego puste pole
+		if(this.projections[day] === undefined || this.projections[day] === null){
+			this.projections[day] = {};
+		}
+		this.projections[day][time] = {	'if3d': if3d,
+										'languageVer': languageVer,
+										'specProjLabel': specProjLabel};
 	}
 }
 
