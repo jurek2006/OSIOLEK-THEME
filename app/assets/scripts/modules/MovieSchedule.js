f170c40901c55b838a65ab9937310b233c2adf96
jurek2006 <jurek2006@gmail.com>
2017-09-15 10:53:34 +0200

    Podstawowe dzialanie movieSchedule - dzialaja przyciski wyboru dnia (przewijanie nie) - dziala ukrywanie - pokazywanie dni repertuaru

--- app/assets/scripts/modules/MovieSchedule.js
+++ app/assets/scripts/modules/MovieSchedule.js
@@ -2,16 +2,46 @@
 class MovieSchedule{
 
 	constructor(){
-		this.scheduleDaysBtns = document.querySelectorAll('.dayPicker__btn--day');
 		this.addBtnsClick();
 	}
 
 	addBtnsClick(){
 	// funkcja dodająca obsługę kliknięcia dnia na dayPickerze (pomijając dni --inactive)
-		this.scheduleDaysBtns.forEach(function(dayBtn){
-			if(!dayBtn.classList.contains('dayPicker__btn--day--inactive')){
+		const daysBtns = document.querySelectorAll('.dayPicker__btn--day');
+		daysBtns.forEach(function(dayBtn){
+			if(!dayBtn.classList.contains('dayPicker__btn--day--disabled')){
 				dayBtn.addEventListener('click', function(){
-					console.log(this);
+					// zapamiętanie aktywnego dotychczas dnia: Btn - czyli przycisk na pickerze i time czyli sama data z atrybutu datetime elementu time - np. 2017-09-13
+			        const deactivDayBtn = document.querySelector('.dayPicker__btn--day--selected');
+			        const deactivDayTime = deactivDayBtn.getElementsByTagName("time")[0].getAttribute("datetime");
+
+			        // pobranie dnia właśnie wybranego: Time czyli sama data z atrybutu datetime elementu time - np. 2017-09-13
+			        const selectedDayTime = this.getElementsByTagName("time")[0].getAttribute("datetime");
+
+					// jeśli wybrano dzień inny niż do tej pory aktywny - ukrycie repertuaru na wcześniej aktywny dzień i wyświetlanie na właśnie wybrany
+					if (selectedDayTime !== deactivDayTime){
+
+			        	// dodanie i odebranie klasy ukrywającej dzień repertuaru       
+				        const dayToHide = document.querySelector('.movieSchedule__ofDay[day="'+ deactivDayTime +'"]');
+				        const dayToShow = document.querySelector('.movieSchedule__ofDay[day="'+ selectedDayTime +'"]');
+
+				        if (dayToHide != null && dayToHide !== undefined && dayToShow != null && dayToShow !== undefined) {
+							// jeśli udało się pobrać article dla odpowiednich dni - wyświetlenie wybranego i ukrycie dotychczasowego, oraz zmiana "wybranego" przycisku       
+							dayToHide.classList.add("movieSchedule__ofDay--hidden");
+							dayToShow.classList.remove("movieSchedule__ofDay--hidden");
+
+							// dezaktywacja przycisku dotychczasowej daty
+							deactivDayBtn.classList.remove("dayPicker__btn--day--selected");
+							// aktywacja przycisku wybranej właśnie daty (kliknięty przycisk to this)
+							this.classList.add("dayPicker__btn--day--selected");
+						} else {
+							console.log("Nie udało się znaleźć odpowiedniego dnia repertuaru");
+						}
+
+			        } else {
+			        	// TESTOWE
+			        	console.log("Wybrano ten sam dzień");
+			        }
 				});
 			}
 		});
@@ -19,6 +49,12 @@ class MovieSchedule{
 
 }
 
-
-
 export default MovieSchedule;
+
+// Do zrobienia: 
+// - osobne definiowanie klass css (żeby łatwo było zmienić)
+// - przenieść eventListenera dla przycisków do elementu nadrzędnego i użyć delegacji (???)
+// - funkcjonalność "przesuwania" dni
+// - responsywność
+// - styl nie tylko za pomocą grid i flexbox
+// - dopracować wygląd
