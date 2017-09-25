cc5a9d90177daf812b1a5555e88830e9c090ecb7
jurek2006 <jurek2006@gmail.com>
2017-09-25 15:13:43 +0200

    Przeniesienie stylów karuzeli slick do _slickCarousel.css, a funkcji uruchamiającej karuzelę do modułu SlickCarousel.js

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,8 +2,12 @@ import $ from 'jquery';
 import MovieSchedule from './modules/MovieSchedule';
 import ModulTestowy from './modules/ModulTestowy';
 import GenerateSchedule from './modules/GenerateSchedule';
+import SlickCarousel from './modules/SlickCarousel';
 
 const movieSchedule = new MovieSchedule();
+
+// SLICK CAROUSEL
+const slickCarousel = new SlickCarousel();
 
 // DEVELOP
 const generateSchedule = new GenerateSchedule();
