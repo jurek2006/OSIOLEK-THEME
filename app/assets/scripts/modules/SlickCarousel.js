ef1289dfe92c81d724e12ae5cea2046928a4b386
jurek2006 <jurek2006@gmail.com>
2017-09-26 13:00:31 +0200

    Wstępnie przygotowany slider sekcji filmy (na pewno trzeba dodać etykiety i poprawić wyświetlanie)

--- app/assets/scripts/modules/SlickCarousel.js
+++ app/assets/scripts/modules/SlickCarousel.js
@@ -1,43 +1,14 @@
-
-
 class SlickCarousel{
     constructor(){
         
         $(document).ready(function(){
-            $('.slick').slick({
+            $('.slickCarousel--filmy').slick({
+              infinite: true,
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              autoplay: true,
               dots: true,
-      infinite: false,
-      speed: 300,
-      slidesToShow: 5,
-      slidesToScroll: 5,
-      responsive: [
-        // {
-        //   breakpoint: 1024,
-        //   settings: {
-        //     slidesToShow: 3,
-        //     slidesToScroll: 3,
-        //     infinite: true,
-        //     dots: true
-        //   }
-        // },
-        // {
-        //   breakpoint: 600,
-        //   settings: {
-        //     slidesToShow: 2,
-        //     slidesToScroll: 2
-        //   }
-        // },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-      ]
+              variableWidth: true,
             });
         });
     }
